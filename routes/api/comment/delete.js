60ad1fb635e77c3c044ce9c20750ef7d4918db22
Ozelot <contact@ozelot.ovh>
2025-05-20 21:30:39 +0200

    Added comment delete endpoint

---
+++ routes/api/comment/delete.js
@@ -0,0 +1,56 @@
+import { prisma } from "../../../index.js";
+
+import config from "../../../config.js";
+
+const rateLimit = new Map();
+
+setInterval(() => {
+    const now = Date.now();
+    for(const [id, start] of rateLimit.entries()) {
+        if(now - start >= config.rateLimit.cooldown) {
+            rateLimit.delete(id);
+        }
+    }
+}, config.rateLimit.interval);
+
+export default async (fastify, options) => {
+
+    fastify.post("/delete", { preHandler: fastify.requireAuthentication }, async (req, res) => {
+
+        if(!req.body) return res.status(400).send({ success: false, error: "Missing body" });
+
+        const { id } = req.body;
+
+        if(!id) return res.status(400).send({ success: false, error: "Missing id" });
+
+        if(rateLimit.has(req.session.user.id)) return res.status(429).send({ success: false, error: "You are being rate-limited" });
+
+        try {
+
+            const comment = await prisma.comments.findFirst({
+                where: { id }
+            });
+
+            if(!comment) return res.status(404).send({ success: false, error: "Comment not found" });
+
+            if(comment.authorId !== req.session.user.id) return res.status(403).send({ success: false, error: "You are not authorized to delete this comment" });
+
+            await prisma.comments.update({
+                where: { id },
+                data: {
+                    hidden: true
+                }
+            });
+
+            rateLimit.set(req.session.user.id, Date.now());
+    
+            return res.send({ success: true });
+
+        } catch(err) {
+            console.error(err);
+            return res.status(500).send({ success: false });
+        }
+
+    });
+
+}
