dd6ee92190b09dbbc1240525b9429545985ac485
Ozelot <contact@ozelot.ovh>
2025-06-08 15:04:38 +0200

    Fixed logout

--- routes/api/logout.js
+++ routes/api/logout.js
@@ -2,7 +2,12 @@ export default async (fastify, options) => {
 
     fastify.post("/logout", async (req, res) => {
 
-        await req.destroySession();
+        await new Promise((resolve, reject) => {
+            req.session.destroy(err => {
+                if(err) reject(err);
+                else resolve();
+            });
+        });
       
         res.clearCookie("sessionId", { path: "/" });
       
